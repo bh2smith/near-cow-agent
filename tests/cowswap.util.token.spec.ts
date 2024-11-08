cec1ba956f133ec023d0470164dec5a5a2de6694
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-11-08 15:07:16 +0100

    fmt

--- tests/cowswap.util.token.spec.ts
+++ tests/cowswap.util.token.spec.ts
@@ -3,13 +3,14 @@ import { zeroAddress } from "viem";
 
 describe("getTokenDetails", () => {
   it("should fail to get token details for zero address", async () => {
-    await expect(getTokenDetails(100, zeroAddress))
-      .rejects
-      .toThrow(); // or .rejects.toThrow("specific error message") if you want to check the message
+    await expect(getTokenDetails(100, zeroAddress)).rejects.toThrow(); // or .rejects.toThrow("specific error message") if you want to check the message
   });
 
   it("should return the token details for a given symbol", async () => {
-    const tokenDetails = await getTokenDetails(100, "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb");
+    const tokenDetails = await getTokenDetails(
+      100,
+      "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
+    );
     expect(tokenDetails).toBeDefined();
   });
 });
