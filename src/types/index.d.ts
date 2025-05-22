323826a80f7b493997f7419403840c6e2e0a17ae
Ruben Marcus <rubenmarcus.dev@gmail.com>
2025-05-22 13:45:26 +0100

    adjust payloads

--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import type { TxData as OriginalTxData } from "@bitte-ai/agent-sdk";
-import type { TransferFTData } from "@bitte-ai/types";
-
-declare module "@bitte-ai/agent-sdk" {
-  interface TxData extends OriginalTxData {
-    data?: TransferFTData;
-  }
-}
