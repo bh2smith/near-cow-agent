961b365c8dd5c9bfa6c08112b39e3b46481fcedb
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-06-07 09:54:09 +0200

    Cleanup unused content

--- src/script/bridge-example.ts
+++ src/script/bridge-example.ts
@@ -1,23 +1,23 @@
-import type { Order } from '@cowprotocol/contracts';
-import { OrderBalance, OrderKind } from '@cowprotocol/contracts';
-import { createOrder } from '../app/lib/bridge/common';
-import { getAddress, type Address, type WalletClient } from 'viem';
-import { formatBytes32String } from '../app/lib/bridge/cow-shed';
-import { gnosisBridgeCommands } from '../app/lib/weiroll/gnosisNativeBridge';
-import { CowShedSdk, type CowShedCall } from '@cowprotocol/cow-sdk';
+import type { Order } from "@cowprotocol/contracts";
+import { OrderBalance, OrderKind } from "@cowprotocol/contracts";
+import { createOrder } from "../app/lib/bridge/common";
+import { getAddress, type Address, type WalletClient } from "viem";
+import { formatBytes32String } from "../app/lib/bridge/cow-shed";
+import { gnosisBridgeCommands } from "../app/lib/weiroll/gnosisNativeBridge";
+import { CowShedSdk, type CowShedCall } from "@cowprotocol/cow-sdk";
 
 const ZERO = BigInt(0);
 interface OrderInfo {
-      buyToken: Address;
-      sellToken: Address; // 0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1 WETH GC
-      buyAmount: bigint;
-      sellAmount: bigint;
-      validity?: number;
-  }
+  buyToken: Address;
+  sellToken: Address; // 0x6a023ccd1ff6f2045c3309768ead9e68f978f6e1 WETH GC
+  buyAmount: bigint;
+  sellAmount: bigint;
+  validity?: number;
+}
 
 export async function swapAndBridgeExample(
   wallet: WalletClient,
-  orderInfo: OrderInfo
+  orderInfo: OrderInfo,
 ): Promise<void> {
   if (!wallet.account) {
     throw new Error("Missing wallet account");
@@ -34,22 +34,25 @@ export async function swapAndBridgeExample(
   // TODO: We need ot split this.
   const cowShedCall = await shed.signCalls({
     chainId,
-    calls: gnosisBridgeCommands(orderInfo.buyToken, shedAccount), 
+    calls: gnosisBridgeCommands(orderInfo.buyToken, shedAccount),
     nonce: formatBytes32String(Date.now().toString()),
   });
 
-  await postSwapAndBridgeOrder(wallet, orderInfo, cowShedCall)
+  await postSwapAndBridgeOrder(wallet, orderInfo, cowShedCall);
 }
 
-
-export async function postSwapAndBridgeOrder(wallet: WalletClient, orderInfo: OrderInfo, {cowShedAccount, signedMulticall, gasLimit}: CowShedCall) {
-  const {to, data} = signedMulticall;
+export async function postSwapAndBridgeOrder(
+  wallet: WalletClient,
+  orderInfo: OrderInfo,
+  { cowShedAccount, signedMulticall, gasLimit }: CowShedCall,
+) {
+  const { to, data } = signedMulticall;
 
   const hooks = {
     post: [{ target: to, callData: data, gasLimit: gasLimit.toString() }],
   };
 
-  const {sellToken, buyToken, sellAmount, buyAmount, validity} = orderInfo;
+  const { sellToken, buyToken, sellAmount, buyAmount, validity } = orderInfo;
   const validTo = validity ?? Math.floor(new Date().getTime() / 1000) + 7200;
   const order: Order = {
     sellToken,
@@ -58,7 +61,7 @@ export async function postSwapAndBridgeOrder(wallet: WalletClient, orderInfo: Or
     sellAmount,
     buyAmount,
     validTo,
-    appData: '',
+    appData: "",
     feeAmount: ZERO,
     kind: OrderKind.SELL,
     partiallyFillable: true,
@@ -68,5 +71,5 @@ export async function postSwapAndBridgeOrder(wallet: WalletClient, orderInfo: Or
 
   // create order
   const orderTx = await createOrder(wallet, order, hooks);
-  console.log('Create order tx', orderTx.transactionHash);
+  console.log("Create order tx", orderTx.transactionHash);
 }
