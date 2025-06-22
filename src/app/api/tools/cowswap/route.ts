af759961940ab86614522954b60d787e760c4d4f
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-06-22 14:12:03 +0200

    Bun Update (#57)

--- src/app/api/tools/cowswap/route.ts
+++ src/app/api/tools/cowswap/route.ts
@@ -4,7 +4,7 @@ import { orderRequestFlow, type OrderResponse } from "./orderFlow";
 import { validateNextRequest, getZerionKey, getTokenMap } from "../util";
 import { handleRequest } from "@bitte-ai/agent-sdk";
 import { parseWidgetData } from "./util/ui";
-import type { BaseSwapFTData } from "@bitte-ai/types";
+import type { SwapFTData } from "@bitte-ai/types";
 
 // Refer to https://api.cow.fi/docs/#/ for Specifics on Quoting and Order posting.
 
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
 async function logic(
   req: NextRequest,
-): Promise<OrderResponse & { data: BaseSwapFTData }> {
+): Promise<OrderResponse & { data: SwapFTData }> {
   const parsedRequest = await parseQuoteRequest(
     req,
     await getTokenMap(),
