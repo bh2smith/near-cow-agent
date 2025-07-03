fe8ebc5e1c108a6308f3f3aa04eb01521f583cf3
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-03 14:07:06 +0200

    Apply Slippage

--- src/app/api/tools/quote/route.ts
+++ src/app/api/tools/quote/route.ts
@@ -5,11 +5,14 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { basicParseQuote } from "../cowswap/util/parse";
 import { getTokenMap } from "../util";
-import { setPresignatureTx } from "../cowswap/util/protocol";
+import { applySlippage, setPresignatureTx } from "../cowswap/util/protocol";
 import { OrderSigningUtils } from "@cowprotocol/cow-sdk";
 import { getAddress } from "viem";
 import type { SignRequest, SwapFTData } from "@bitte-ai/types";
 import { parseWidgetData } from "../cowswap/util/ui";
+
+// TODO: Allow User to set Slippage.
+const slippageBps = Number.parseInt(process.env.SLIPPAGE_BPS || "100");
 
 import { sellTokenApprovalTx } from "@/src/app/api/tools/cowswap/util/protocol";
 export async function POST(req: NextRequest): Promise<NextResponse> {
@@ -25,15 +28,17 @@ async function logic(req: NextRequest): Promise<{
   console.log("Parsed Quote Request", parsedRequest);
   const orderBookApi = new OrderBookApi({ chainId: parsedRequest.chainId });
 
-  const { quote, from, expiration, verified } = await orderBookApi.getQuote(
-    parsedRequest.quoteRequest,
-  );
+  const result = await orderBookApi.getQuote(parsedRequest.quoteRequest);
+  const { from, expiration, verified } = result;
+  // This needs to be altered.
+  let quote = result.quote;
   console.log("POST Response for quote:", quote);
   const { chainId } = parsedRequest;
   if (from === undefined) {
     throw new Error("owner unspecified");
   }
 
+  // TODO(bh2smith): Check For approval before quote Or also return quote with approval somehow.
   const owner = getAddress(from);
   const approvalTx = await sellTokenApprovalTx({
     from: owner,
@@ -51,14 +56,20 @@ async function logic(req: NextRequest): Promise<{
       meta: "user must approve token before continuing",
     };
   }
-  // Set Referral Code.
-  quote.appData =
-    "0x5a8bb9f6dd0c7f1b4730d9c5a811c2dfe559e67ce9b5ed6965b05e59b8c86b80";
+
   const { sellAmount, feeAmount } = quote;
-  // Adjust the sellAmount to account for the fee.
-  // cf: https://learn.cow.fi/tutorial/submit-order
-  quote.sellAmount = (BigInt(sellAmount) + BigInt(feeAmount)).toString();
-  quote.feeAmount = "0";
+  quote = {
+    ...quote,
+    // Apply Slippage based on OrderKind
+    ...applySlippage(quote, slippageBps),
+    // Adjust the sellAmount to account for the fee.
+    // cf: https://learn.cow.fi/tutorial/submit-order
+    sellAmount: (BigInt(sellAmount) + BigInt(feeAmount)).toString(),
+    feeAmount: "0",
+    // Set Referral Code.
+    appData:
+      "0x5a8bb9f6dd0c7f1b4730d9c5a811c2dfe559e67ce9b5ed6965b05e59b8c86b80",
+  };
 
   const { orderId, orderDigest } = await OrderSigningUtils.generateOrderId(
     chainId,
