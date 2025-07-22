8772511a0cf0f7f804863030fda86038a33c0636
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-22 13:36:10 +0200

    Improve Price & Icon Data

--- src/lib/ui.ts
+++ src/lib/ui.ts
@@ -1,9 +1,14 @@
 import { getChainById } from "@bitte-ai/agent-sdk";
 import { formatUnits } from "viem";
+import { ZerionAPI } from "zerion-sdk";
+
+import { getZerionKey } from "../app/config";
+import { CHAIN_ICONS } from "./ui/icon";
 
 import type { TokenInfo } from "@bitte-ai/agent-sdk";
 import type { SwapFTData } from "@bitte-ai/types";
 import type { OrderParameters } from "@cowprotocol/cow-sdk";
+import type { Address } from "viem";
 
 interface SwapDetails {
   chainId: number;
@@ -11,29 +16,57 @@ interface SwapDetails {
   quote: OrderParameters;
 }
 
-export function parseWidgetData({
+export async function parseWidgetData({
   chainId,
   tokenData,
   quote,
-}: SwapDetails): SwapFTData {
+}: SwapDetails): Promise<SwapFTData> {
   const chain = getChainById(chainId);
+  // TODO: Enable Price Agent
+  // const [sellPrice, buyPrice] = await Promise.all([
+  //   externalPriceFeed({ chainId, address: quote.sellToken }),
+  //   externalPriceFeed({ chainId, address: quote.buyToken }),
+  // ]);
+  const zerion = new ZerionAPI(getZerionKey());
+  const [sellData, buyData] = await Promise.all([
+    zerion.getToken({ chainId, address: quote.sellToken as Address }),
+    zerion.getToken({ chainId, address: quote.buyToken as Address }),
+  ]);
+
+  const sellPrice = sellData.attributes.market_data.price;
+  const buyPrice = buyData.attributes.market_data.price;
+
+  console.log(
+    `Retrieved Prices: sellTokenPrice:${sellPrice}, buyTokenPrice:${buyPrice}`,
+  );
+  const sellAmount = formatUnits(
+    BigInt(quote.sellAmount),
+    tokenData.sell.decimals,
+  );
+  const buyAmount = formatUnits(
+    BigInt(quote.buyAmount),
+    tokenData.buy.decimals,
+  );
+
   return {
     network: {
       name: chain.name,
-      icon: "",
+      icon: CHAIN_ICONS[chainId] ?? "",
     },
     type: "swap",
     fee: quote.feeAmount,
     tokenIn: {
       contractAddress: quote.sellToken,
-      amount: formatUnits(BigInt(quote.sellAmount), tokenData.sell.decimals),
-      usdValue: 0,
+      amount: sellAmount,
+      usdValue: parseFloat(sellAmount) * (sellPrice ?? 0),
+      icon: sellData.attributes.icon.url,
       ...tokenData.sell,
     },
     tokenOut: {
       contractAddress: quote.buyToken,
-      amount: formatUnits(BigInt(quote.buyAmount), tokenData.buy.decimals),
-      usdValue: 0,
+      amount: buyAmount,
+      usdValue: parseFloat(buyAmount) * (buyPrice ?? 0),
+      icon: buyData.attributes.icon.url,
       ...tokenData.buy,
     },
   };
