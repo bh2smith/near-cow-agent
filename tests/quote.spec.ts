8cb687b300be8c7c1882e75bec30d745a52ee4b6
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-08-06 08:26:26 -0400

    Fix Buy & Sell ETH Bugs (#96)
    **1. Fix Buy ETH:** 
    Bump Zerion SDK for ETH price data fetching. #95
    - See [Local Test
    Order](https://explorer.cow.fi/base/orders/0x033d6df2b7bab11df55ec007fc020fdbca52da3cef089fb210c7914faac1219ab00b4c1e371dee4f6f32072641430656d3f7c06468813a57)
    
    **2. Fix Sell ETH:** 
    Modify the quote request with wrapped native sell before the quote is
    made #97
    
    cc @tifrel - I would like to merge this and update the manifest.

---
+++ tests/quote.spec.ts
@@ -0,0 +1,33 @@
+import { handleQuoteRequest } from "@/src/app/api/tools/quote/logic";
+import { OrderQuoteRequest, TokenInfo } from "@cowprotocol/cow-sdk";
+
+describe.skip("Quote Route Logic", () => {
+  it("should transform parsedQuoteRequest into Full Quote", async () => {
+    const quoteRequest = {
+      sellToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // ETH
+      buyToken: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      sellAmountBeforeFee: "100000000000000",
+      kind: "sell",
+      receiver: "0xB00b4C1e371DEe4F6F32072641430656D3F7c064",
+      from: "0xB00b4C1e371DEe4F6F32072641430656D3F7c064",
+      signingScheme: "eip712",
+    } as OrderQuoteRequest;
+    const tokenData = {
+      sell: {
+        address: quoteRequest.sellToken,
+        decimals: 18,
+        symbol: "ETH",
+        name: "Ether",
+      },
+      buy: {
+        address: quoteRequest.buyToken,
+        decimals: 6,
+        symbol: "USDC",
+        name: "Circles USD",
+      },
+    };
+    const input = { chainId: 8453, quoteRequest, tokenData, slippageBps: 100 };
+    // @ts-expect-error: tokenData isn't happy.
+    expect(handleQuoteRequest(input)).resolves.toBeDefined();
+  });
+});
