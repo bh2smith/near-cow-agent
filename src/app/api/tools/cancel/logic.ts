4b5d2030a1efcdea4ec20f73bcdd3f5dc76aa5a2
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-08-07 13:55:40 -0400

    Untested cancelation request path

--- src/app/api/tools/cancel/logic.ts
+++ src/app/api/tools/cancel/logic.ts
@@ -1,12 +1,17 @@
-import { EcdsaSigningScheme, OrderBookApi } from "@cowprotocol/cow-sdk";
+import {
+  EcdsaSigningScheme,
+  OrderBookApi,
+  OrderSigningUtils,
+} from "@cowprotocol/cow-sdk";
 
 import { withCowErrorHandling } from "@/src/lib/error";
 import { CancelOrderSchema, parseRequest } from "@/src/lib/schema";
 
 import type { CancelOrderInput } from "@/src/lib/schema";
+import type { SignRequest } from "@bitte-ai/types";
 import type { NextRequest } from "next/server";
 
-export async function logic(req: NextRequest): Promise<void> {
+export async function logic(req: NextRequest): Promise<SignRequest | void> {
   const data = parseRequest(req, CancelOrderSchema);
   return handleCancellationRequest(data);
 }
@@ -15,14 +20,11 @@ export async function handleCancellationRequest({
   chainId,
   orderUid,
   signature,
-}: CancelOrderInput): Promise<void> {
+}: CancelOrderInput): Promise<SignRequest | void> {
   const orderBookApi = new OrderBookApi({ chainId });
 
   if (!signature) {
-    // 1. Check for order: This will throw with not found if order doesn't exist.
-    await withCowErrorHandling(orderBookApi.getOrderMultiEnv(orderUid));
-    // 2. Create Signable Order Cancellation Payload.
-    // TODO
+    return buildCancelOrderData(orderBookApi, orderUid);
   } else {
     return orderBookApi.sendSignedOrderCancellations({
       orderUids: [orderUid],
@@ -30,4 +32,31 @@ export async function handleCancellationRequest({
       signingScheme: EcdsaSigningScheme.EIP712,
     });
   }
+}
+
+export async function buildCancelOrderData(
+  orderbook: OrderBookApi,
+  orderUid: string,
+): Promise<SignRequest> {
+  const chainId = orderbook.context.chainId;
+  // 1. Check for order: This will throw with not found if order doesn't exist.
+  const order = await withCowErrorHandling(
+    orderbook.getOrderMultiEnv(orderUid),
+  );
+  // 2. Create Signable Order Cancellation Payload.
+  const typedData = {
+    domain: OrderSigningUtils.getDomain(chainId),
+    types: {
+      OrderCancellations: [{ name: "orderUids", type: "bytes[]" }],
+    },
+    primaryType: "OrderCancellations",
+    message: {
+      orderUids: [orderUid],
+    },
+  };
+  return {
+    method: "eth_signTypedData_v4",
+    chainId,
+    params: [order.owner as `0x${string}`, JSON.stringify(typedData)],
+  };
 }
