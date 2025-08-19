6745e98a6f7c02cecbe2b9aebe214833953b2737
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-08-19 14:50:44 +0200

    Functional Status Route without tests

---
+++ src/app/api/tools/status/logic.ts
@@ -0,0 +1,33 @@
+import { OrderBookApi } from "@cowprotocol/cow-sdk";
+
+import { withCowErrorHandling } from "@/src/lib/error";
+import { OrderStatusSchema, parseRequest } from "@/src/lib/schema";
+
+import type { OrderStatusInput } from "@/src/lib/schema";
+import type { CompetitionOrderStatus, OrderStatus } from "@cowprotocol/cow-sdk";
+import type { NextRequest } from "next/server";
+
+type StatusResponse = {
+  order: OrderStatus;
+  competition: CompetitionOrderStatus;
+};
+
+export async function logic(req: NextRequest): Promise<StatusResponse> {
+  const data = parseRequest(req, OrderStatusSchema);
+  return handleOrderStatusRequest(data);
+}
+
+export async function handleOrderStatusRequest(
+  input: OrderStatusInput,
+): Promise<StatusResponse> {
+  const orderbook = new OrderBookApi({ chainId: input.chainId });
+  // Get order: will throw with not found if order doesn't exist.
+  const order = await withCowErrorHandling(
+    orderbook.getOrderMultiEnv(input.orderUid),
+  );
+  // Since the above didn't throw =>
+  // can retrive competition status because we know the order exists.
+  const competition = await orderbook.getOrderCompetitionStatus(input.orderUid);
+  console.log("Competition Status", competition);
+  return { order: order.status, competition };
+}
