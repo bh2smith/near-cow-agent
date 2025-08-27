235e3b7c3531390880938541e0da7004521dfdce
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-08-27 11:47:03 +0200

    Add Order History Tool (missing Response Schema) (#124)
    Closes #121 
    
    Adding an agent tool to retrieve order history.
    
    Define response schema as an array of the following datum:
    
    ```ts
      {
        "creationDate": "2025-07-26T13:56:16.984610Z",
        "owner": "0xb00b4c1e371dee4f6f32072641430656d3f7c064",
        "uid": "0xf944f0de097e6c3724ebbd4e18881df348ca6e8fc388b322e76b5aa139a819fcb00b4c1e371dee4f6f32072641430656d3f7c0646884e57b",
        "availableBalance": null,
        "executedBuyAmount": "95155864",
        "executedSellAmount": "25464867331308488",
        "executedSellAmountBeforeFees": "25464867331308488",
        "executedFeeAmount": "0",
        "executedFee": "47061046759568",
        "executedFeeToken": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
        "invalidated": false,
        "status": "fulfilled",
        "class": "limit",
        "settlementContract": "0x9008d19f58aabd9ed0d60971565aa8510560ab41",
        "isLiquidityOrder": false,
        "fullAppData": "{\"appCode\":\"CoW Swap\",\"environment\":\"production\",\"metadata\":{\"orderClass\":{\"orderClass\":\"market\"},\"quote\":{\"slippageBips\":10,\"smartSlippage\":false},\"utm\":{\"utmContent\":\"menubar-nav-button-trade-on-cow-swap\",\"utmMedium\":\"web\",\"utmSource\":\"cow.fi\"}},\"version\":\"1.4.0\"}",
        "sellToken": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
        "buyToken": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
        "receiver": "0xb00b4c1e371dee4f6f32072641430656d3f7c064",
        "sellAmount": "25464867331308488",
        "buyAmount": "95075165",
        "validTo": 1753539963,
        "appData": "0xcd31169a713a0f3aa6eb9e4f47b7294aa12668db346f54d34199b112b0f86509",
        "feeAmount": "0",
        "kind": "sell",
        "partiallyFillable": false,
        "sellTokenBalance": "erc20",
        "buyTokenBalance": "erc20",
        "signingScheme": "eip712",
        "signature": "0x6e9fd6592280e03626a55b83087e1c8440942b1a055fa80686c412904ebeb4393871c675ccd7232253aa098cf92f78487d170f094b3b37dde588caa6b562c05a1c",
        "interactions": {
          "pre": [],
          "post": []
        },
        "totalFee": "47061046759568"
      }
      ```

---
+++ src/app/api/tools/history/logic.ts
@@ -0,0 +1,25 @@
+import { OrderBookApi } from "@cowprotocol/cow-sdk";
+
+import { withCowErrorHandling } from "@/src/lib/error";
+import {
+  type OrderHistoryInput,
+  OrderHistorySchema,
+  parseRequest,
+} from "@/src/lib/schema";
+
+import type { Order } from "@cowprotocol/cow-sdk";
+import type { NextRequest } from "next/server";
+
+export async function logic(req: NextRequest): Promise<Order[]> {
+  const data: OrderHistoryInput = parseRequest(req, OrderHistorySchema);
+  return getOrderHistory(data);
+}
+
+export async function getOrderHistory({
+  chainId, // TODO: Allow undefined and fetch for all supported networks.
+  evmAddress,
+}: OrderHistoryInput): Promise<Order[]> {
+  const orderBookApi = new OrderBookApi({ chainId });
+
+  return withCowErrorHandling(orderBookApi.getOrders({ owner: evmAddress }));
+}
