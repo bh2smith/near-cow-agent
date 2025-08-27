092711d4da305b620730fe1d99edaa67a0eb0c0e
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-08-27 15:58:19 +0200

    Name was already included

--- tests/ui.spec.ts
+++ tests/ui.spec.ts
@@ -1,4 +1,6 @@
 import { getPriceAndIcon, parseWidgetData } from "@/src/lib/ui";
+import { getTokenDetails } from "@bitte-ai/agent-sdk";
+import { TokenInfo } from "@bitte-ai/types";
 import { OrderKind, OrderParameters } from "@cowprotocol/cow-sdk";
 import { getAddress } from "viem";
 
@@ -6,7 +8,7 @@ const GNOSIS_GNO = getAddress("0x9c58bacc331c9aa871afd802db6379a98e80cedb");
 const GNOSIS_COW = getAddress("0x177127622c4a00f3d409b75571e12cb3c8973d3c");
 
 const chainId = 100;
-const tokenData = {
+const tokenDataFixture = {
   sell: {
     address: GNOSIS_GNO,
     decimals: 18,
@@ -23,17 +25,21 @@ const tokenData = {
 
 describe("getPriceAndIcon", () => {
   it.skip("known tokens", async () => {
-    let data = await getPriceAndIcon({ chainId, ...tokenData.sell });
+    let data = await getPriceAndIcon({ chainId, ...tokenDataFixture.sell });
     console.log(data);
 
-    data = await getPriceAndIcon({ chainId, ...tokenData.buy });
+    data = await getPriceAndIcon({ chainId, ...tokenDataFixture.buy });
     console.log(data);
   });
 
-  it.skip("parseSwapData", async () => {
+  it("parseSwapData", async () => {
+    const tokenData = {
+      buy: await getTokenDetails(chainId, GNOSIS_COW),
+      sell: await getTokenDetails(chainId, GNOSIS_GNO),
+    };
     const quote: OrderParameters = {
-      sellToken: tokenData.sell.address,
-      buyToken: tokenData.buy.address,
+      sellToken: tokenData.sell!.address,
+      buyToken: tokenData.buy!.address,
       sellAmount: "123456789101112131415161718192345",
       buyAmount: "9876543234567",
       validTo: 0,
@@ -44,31 +50,10 @@ describe("getPriceAndIcon", () => {
     };
     const swapData = await parseWidgetData({
       chainId: 100,
+      feeAmount: "1234",
       tokenData,
       quote,
     });
     console.log(JSON.stringify(swapData, null, 2));
-    // expect(swapData).toStrictEqual({
-    //   network: {
-    //     name: "Gnosis",
-    //     icon: "https://storage.googleapis.com/bitte-public/intents/chains/gnosis.svg",
-    //   },
-    //   type: "swap",
-    //   fee: "123",
-    //   tokenIn: {
-    //     contractAddress: tokenData.sell.address,
-    //     amount: "123456789101112.131415161718192345",
-    //     usdValue: 0,
-    //     icon: "https://cdn.zerion.io/0x6810e776880c02933d47db1b9fc05908e5386b96.png",
-    //     ...tokenData.sell,
-    //   },
-    //   tokenOut: {
-    //     contractAddress: tokenData.buy.address,
-    //     amount: "9876543.234567",
-    //     icon: "https://cdn.zerion.io/0xdef1ca1fb7fbcdc777520aa7f396b4e015f497ab.png",
-    //     usdValue: 0,
-    //     ...tokenData.buy,
-    //   },
-    // });
   });
 });
